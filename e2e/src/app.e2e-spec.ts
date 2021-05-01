7a3757a9415b5403c8c58bcf5aa5879820647799
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-01 22:38:42 +0200

    Add bootstrap and bootstrap-icons

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('kaffee-kasse-frontend app is running!');
+    expect(await page.getTitleText()).toEqual('kaffee-kasse-frontend');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('kaffee-kasse-frontend app is running!');
+    expect(compiled.querySelector('header h1').textContent).toContain('kaffee-kasse-frontend');
   });
 });
