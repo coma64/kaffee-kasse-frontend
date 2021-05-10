0f1b554a5fc74df66c5c88a6f3477c0ed99bc1de
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-10 17:08:36 +0200

    Remove stupid AppComponent route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '@components/login/login.component';
-import { AuthGuard } from '@guards/auth/auth.guard';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', component: AppComponent, canActivate: [AuthGuard] }
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
