ae52e8524d6a2cc881277025c9a945c97971e01d
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-10 16:22:58 +0200

    Add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '@components/login/login.component';
+import { AuthGuard } from '@guards/auth/auth.guard';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: AppComponent }
+  { path: '', component: AppComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
---
+++ src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { AuthService } from '@services/auth/auth.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const credentials = this.authService.credentials;
+    if (credentials !== undefined)
+      return true;
+
+    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+    return false;
+  }
+}
