f50eaf5c760336f257b0fbbb975d6e0ee2ecdaca
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-10 17:23:25 +0200

    Add unauthorized interceptor

---
+++ src/app/interceptors/unauthorized/unauthorized.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '@services/auth/auth.service';
+
+@Injectable()
+export class UnauthorizedInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((err: HttpErrorResponse) => {
+        if (err.status === 401)
+          this.authService.logout();
+
+        return throwError(err);
+      })
+    );
+  }
+}
