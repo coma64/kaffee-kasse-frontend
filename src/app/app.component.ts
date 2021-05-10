2f76322f5918757d5e146e0ef243eb0e78e599e0
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-10 15:05:18 +0200

    Add auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AuthService } from '@services/auth/auth.service';
+import { Collapse } from 'bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kaffee-kasse-frontend';
+  @ViewChild('navbarToggler') navbarTogglerElement?: HTMLElement;
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    if (this.navbarTogglerElement)
+      new Collapse(this.navbarTogglerElement);
+    this.authService.credentials$.subscribe(creds => console.log(creds, 'creds sub'));
+  }
+
+  login(username: string, password: string) {
+    this.authService.login(username, password).subscribe(
+      loggedIn => console.log(loggedIn)
+    );
+  }
+
+  logUser() {
+    console.log(this.authService.credentials);
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,61 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Credentials } from '@models/credentials';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private credentialsSubject: BehaviorSubject<Credentials | undefined>;
+  credentials$: Observable<Credentials | undefined>;
+
+  constructor(private router: Router, private http: HttpClient) {
+    this.credentialsSubject = new BehaviorSubject<Credentials | undefined>(
+      localStorage.getItem('credentials') !== null
+        ? JSON.parse(localStorage.getItem('credentials') as string)
+        : undefined
+    );
+    this.credentials$ = this.credentialsSubject.asObservable();
+  }
+
+  public get credentials(): Credentials | undefined {
+    return this.credentialsSubject.value;
+  }
+
+  login(username: string, password: string): Observable<boolean> {
+    return this.http
+      .post<{ token?: string; non_field_errors?: string[], username?: string[], password?: string[] }>(
+        '/api-token-auth/',
+        { username, password }
+      )
+      .pipe(
+        map((response) => {
+          if (response.token === undefined) return false;
+
+          const credentials: Credentials = {
+            token: response.token
+          };
+          localStorage.setItem('credentials', JSON.stringify(credentials));
+          this.credentialsSubject.next(credentials);
+
+          return true;
+        }),
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 400)
+            return of(false);
+
+          return throwError(error);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem('credentials');
+    this.credentialsSubject.next(undefined);
+
+    // this.router.navigate(['/login']);
+  }
+}
