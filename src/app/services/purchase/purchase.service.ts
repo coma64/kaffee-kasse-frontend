b012b6f58d77d476f11cbc744726bad33e4c378d
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-15 12:52:10 +0200

    Add get single purchase function

--- src/app/services/purchase/purchase.service.ts
+++ src/app/services/purchase/purchase.service.ts
@@ -2,7 +2,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Purchase } from '@models/purchase';
+import { User } from '@models/user';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +19,22 @@ export class PurchaseService {
 
   createPurchase(purchase: Purchase): Observable<Purchase> {
     return this.http.post<Purchase>(`${this.purchasesUrl}/`, purchase, this.httpOptions);
+  }
+
+  getPurchases(user?: User): Observable<Purchase[]> {
+    let url = '';
+    if (user !== undefined)
+      url = `${this.purchasesUrl}/?user=${user.id}`;
+    else
+      url = `${this.purchasesUrl}/`;
+
+    return this.http.get<Purchase[]>(url).pipe(
+      map(purchases => purchases.map(this.parseDate))
+    )
+  }
+
+  private parseDate(purchase: Purchase): Purchase {
+    purchase.date = new Date(purchase.date);
+    return purchase;
   }
 }
