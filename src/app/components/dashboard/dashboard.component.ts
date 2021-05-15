bdab47e18bea784b0b2981b8e9aafe61565f18c0
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-15 12:53:05 +0200

    Display purchases in dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { environment } from '@environments/environment';
 import { BeverageType } from '@models/beverage-type';
 import { Profile } from '@models/profile';
 import { Purchase } from '@models/purchase';
+import { User } from '@models/user';
+import { AuthService } from '@services/auth/auth.service';
 import { BeverageTypeService } from '@services/beverageType/beverage-type.service';
 import { ProfileService } from '@services/profile/profile.service';
 import { PurchaseService } from '@services/purchase/purchase.service';
 import { UserService } from '@services/user/user.service';
-import { forkJoin, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { forkJoin, Observable, of } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,6 +21,7 @@ import { switchMap } from 'rxjs/operators';
 export class DashboardComponent implements OnInit {
   currentProfile?: Profile;
   beverageTypes: BeverageType[] = [];
+  purchases: Purchase[] = [];
   purchaseForm = new FormGroup({
     beverageType: new FormControl(),
     amount: new FormControl(1),
@@ -30,23 +33,18 @@ export class DashboardComponent implements OnInit {
     private userService: UserService,
     private profileService: ProfileService,
     private purchaseService: PurchaseService,
-    private beverageTypeService: BeverageTypeService
+    private beverageTypeService: BeverageTypeService,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
-    this.userService.currentUser$
-      .pipe(
-        switchMap((user) =>
-          user?.profile !== undefined
-            ? this.profileService.getProfile(user.profile)
-            : of(undefined)
-        )
-      )
-      .subscribe((profile) => (this.currentProfile = profile));
+    this.refreshProfile().subscribe();
 
     this.beverageTypeService
       .getBeverageTypes()
       .subscribe((beverageTypes) => (this.beverageTypes = beverageTypes));
+
+    this.refreshPurchases().subscribe();
   }
 
   createPurchase(): void {
@@ -69,19 +67,37 @@ export class DashboardComponent implements OnInit {
 
     forkJoin(purchases)
       .pipe(
-        switchMap(() => this.userService.currentUser$),
-        switchMap((user) =>
-          user?.profile !== undefined
-            ? this.profileService.getProfile(user.profile)
-            : of(undefined)
-        )
+        switchMap(() => this.refreshProfile()),
+        switchMap(() => this.refreshPurchases()),
       )
-      .subscribe((profile) => {
-        this.currentProfile = profile;
+      .subscribe(() => {
         this.purchaseForm.setValue({
           amount: 1,
           beverageType: null,
         });
       });
+  }
+
+  private refreshProfile(): Observable<Profile> {
+    return this.userService.currentUser$
+      .pipe(
+        filter(user => user != undefined),
+        switchMap((user) => this.profileService.getProfile(user!.profile) as Observable<Profile>),
+        tap(profile => this.currentProfile = profile)
+      );
+  }
+
+  private refreshPurchases(): Observable<Purchase[]> {
+    return this.userService.currentUser$
+      .pipe(
+        filter(user => user != undefined),
+        switchMap((user) =>
+          this.purchaseService.getPurchases(user as User)),
+        tap(purchases => this.purchases = purchases)
+      );
+  }
+
+  getPurchaseBeverageType(purchase: Purchase): BeverageType {
+    return this.beverageTypes[this.beverageTypeService.getBeverageTypeIdByUrl(purchase.beverage_type) - 1]
   }
 }
