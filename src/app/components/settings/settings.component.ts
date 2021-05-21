081b7930f1aa8e44ef0a850ad1a74906978adb4a
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-05-21 11:54:34 +0200

    Display current user user detail in settings

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '@models/user';
+import { UserService } from '@services/user/user.service';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
 })
-export class SettingsComponent {}
+export class SettingsComponent implements OnInit {
+  currentUser?: User;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.currentUser$.subscribe(
+      (user) => (this.currentUser = user)
+    );
+  }
+}
