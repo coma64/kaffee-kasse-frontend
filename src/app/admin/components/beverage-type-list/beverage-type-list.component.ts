19bb147aac1963317b24404651ca895135c77aad
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-06-02 23:11:51 +0200

    Remove dead stylesheet reference

--- src/app/admin/components/beverage-type-list/beverage-type-list.component.ts
+++ src/app/admin/components/beverage-type-list/beverage-type-list.component.ts
@@ -3,13 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-beverage-type-list',
   templateUrl: './beverage-type-list.component.html',
-  styleUrls: ['./beverage-type-list.component.scss']
 })
 export class BeverageTypeListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
