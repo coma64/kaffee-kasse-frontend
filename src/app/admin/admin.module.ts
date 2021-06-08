0df9443b5c290d5fe8ea67d6f6f9eef12540d2e3
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-06-08 12:26:27 +0200

    Add beverage type form

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { BeverageTypeListComponent } from './components/beverage-type-list/beverage-type-list.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    BeverageTypeListComponent
-  ],
-  imports: [
-    CommonModule,
-    AdminRoutingModule
-  ]
+  declarations: [BeverageTypeListComponent],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, AdminRoutingModule],
 })
-export class AdminModule { }
+export class AdminModule {}
--- src/app/admin/components/beverage-type-list/beverage-type-list.component.ts
+++ src/app/admin/components/beverage-type-list/beverage-type-list.component.ts
@@ -1,11 +1,128 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BeverageType } from '@models/beverage-type';
+import { BeverageTypeService } from '@services/beverageType/beverage-type.service';
+import { forkJoin } from 'rxjs';
+import { switchMapTo } from 'rxjs/operators';
 
 @Component({
   selector: 'app-beverage-type-list',
   templateUrl: './beverage-type-list.component.html',
+  styleUrls: ['./beverage-type-list.component.scss'],
 })
 export class BeverageTypeListComponent implements OnInit {
-  constructor() {}
+  private beverageTypes?: BeverageType[];
 
-  ngOnInit(): void {}
+  beverageTypesForm?: FormArray;
+
+  constructor(
+    private beverageTypeService: BeverageTypeService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.beverageTypeService.getBeverageTypes().subscribe((beverageTypes) => {
+      this.beverageTypes = beverageTypes;
+      this.initBeverageTypeForm();
+    });
+  }
+
+  private initBeverageTypeForm(): void {
+    if (this.beverageTypes == undefined) return;
+
+    this.beverageTypesForm = this.formBuilder.array(
+      this.beverageTypes.map((beverageType) =>
+        this.formBuilder.group({
+          id: [beverageType.id],
+          name: [beverageType.name, Validators.required],
+          price: [beverageType.price.toFixed(2), Validators.required],
+          shouldBeDeleted: [false],
+        })
+      )
+    );
+  }
+
+  get beverageTypesFormControls(): FormGroup[] | undefined {
+    return this.beverageTypesForm?.controls as FormGroup[] | undefined;
+  }
+
+  onSubmit(): void {
+    if (
+      this.beverageTypesForm == undefined ||
+      this.beverageTypesFormControls == undefined ||
+      this.beverageTypes == undefined ||
+      this.beverageTypesForm.invalid ||
+      this.beverageTypesForm.length === 0
+    )
+      return;
+
+    const parseControlValue = (control: FormGroup): BeverageType => {
+      return {
+        id: parseInt(control.value.id),
+        name: control.value.name,
+        price: parseFloat(control.value.price),
+      };
+    };
+
+    const beverageTypeUpdates = this.beverageTypesFormControls
+      .filter((control) => control.touched && !isNaN(control.value.id))
+      .map((control) => {
+        const beverageType = parseControlValue(control);
+
+        return this.beverageTypeService.updateBeverageType(beverageType);
+      });
+
+    const beverageTypeCreations = this.beverageTypesFormControls
+      .filter(
+        (control) =>
+          control.touched &&
+          isNaN(control.value.id) &&
+          !control.value.shouldBeDeleted
+      )
+      .map((control) => {
+        const beverageType = parseControlValue(control);
+
+        return this.beverageTypeService.createBeverageType(beverageType);
+      });
+
+    const beverageTypeDeletions = this.beverageTypesFormControls
+      .filter(
+        (control) => control.value.shouldBeDeleted && !isNaN(control.value.id)
+      )
+      .map((control) => {
+        const beverageType = parseControlValue(control);
+
+        return this.beverageTypeService.deleteBeverageType(beverageType.id);
+      });
+
+    forkJoin(
+      Array.prototype.concat(
+        beverageTypeUpdates,
+        beverageTypeCreations,
+        beverageTypeDeletions
+      )
+    )
+      .pipe(switchMapTo(this.beverageTypeService.getBeverageTypes()))
+      .subscribe((beverageTypes) => {
+        this.beverageTypes = beverageTypes;
+        this.initBeverageTypeForm();
+      });
+  }
+
+  onReset(): void {
+    this.initBeverageTypeForm();
+  }
+
+  addBeverageTypeForm(): void {
+    if (this.beverageTypesForm == undefined) return;
+
+    const control = this.formBuilder.group({
+      name: ['', Validators.required],
+      price: ['0.00', Validators.required],
+      shouldBeDeleted: [false],
+    });
+    control.markAllAsTouched();
+
+    this.beverageTypesForm.push(control);
+  }
 }
