d0129dd9e2bd3bd3943d6e28b2348887e07f04e5
Tristan Zaton <50082122+coma64@users.noreply.github.com>
2021-06-14 14:53:22 +0200

    Use urlcat for purchase service, shorten names

--- src/app/services/purchase/purchase.service.ts
+++ src/app/services/purchase/purchase.service.ts
@@ -2,40 +2,42 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Purchase } from '@models/purchase';
-import { User } from '@models/user';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import urlcat from 'urlcat';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PurchaseService {
-  private readonly purchasesUrl = `${environment.apiUrl}/purchases`;
+  private readonly url = `${environment.apiUrl}/purchases/`;
+  private readonly countsUrl = `${environment.apiUrl}/purchases/counts/`;
   private readonly httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
   constructor(private http: HttpClient) {}
 
-  createPurchase(purchase: Purchase): Observable<Purchase> {
-    return this.http.post<Purchase>(
-      `${this.purchasesUrl}/`,
-      purchase,
-      this.httpOptions
-    );
+  create(purchase: Purchase): Observable<Purchase> {
+    return this.http
+      .post<Purchase>(this.url, purchase, this.httpOptions)
+      .pipe(map(this.parsePurchase));
   }
 
-  getPurchases(user?: User): Observable<Purchase[]> {
-    let url = '';
-    if (user !== undefined) url = `${this.purchasesUrl}/?user=${user.id}`;
-    else url = `${this.purchasesUrl}/`;
+  getList(
+    options: { userId?: number; order?: string } = {}
+  ): Observable<Purchase[]> {
+    const url = urlcat(this.url, {
+      user: options?.userId,
+      order: options?.order,
+    });
 
     return this.http
       .get<Purchase[]>(url)
-      .pipe(map((purchases) => purchases.map(this.parseDate)));
+      .pipe(map((purchases) => purchases.map(this.parsePurchase)));
   }
 
-  private parseDate(purchase: Purchase): Purchase {
+  }
+
+  private parsePurchase(purchase: Purchase): Purchase {
     purchase.date = new Date(purchase.date);
     return purchase;
   }
